aeef42a4ad863f4b58e8abed30f3254e50284446
niamulhasan <niamulhasanbd@gmail.com>
2019-03-14 01:43:17 +0600

    Also added a TextView to show the Message

--- src/main/java/me/niamulhasan/tutorial/toastfromtextview/MainActivity.java
+++ src/main/java/me/niamulhasan/tutorial/toastfromtextview/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,10 +15,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //This method will called on clicking on the Button. Set in the xml file
     public void showToast(View view){
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
+        EditText editText = (EditText) findViewById(R.id.editText);     //Setting up variable for the editText
+        String message = editText.getText().toString();                 //Getting the value from the editText
 
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show(); //Making the toast with the value
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+
+
     }
 }
